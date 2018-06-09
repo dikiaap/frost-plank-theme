6325b63a0c09c464bfd7d6fc4679d2cb39de26dd
Diki Ananta <diki1aap@gmail.com>
2018-06-09 23:10:30 +0700

    Improve installation script
    - read without -r will mangle backslashes.[0]
    - Double quote to prevent globbing and word splitting.[1]
    
    [0] https://github.com/koalaman/shellcheck/wiki/SC2162
    [1] https://github.com/koalaman/shellcheck/wiki/SC2086

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-read -p "Do you want to install Frost [Y/n]? " response
+read -rp "Do you want to install Frost [Y/n]? " response
 
 case "$response" in
     [yY][eE][sS]|[yY]|"")
         echo "Copying plank theme..."
-        mkdir -p $HOME/.local/share/plank/themes
-        cp -Ri Frost $HOME/.local/share/plank/themes
+        mkdir -p "$HOME"/.local/share/plank/themes
+        cp -Ri Frost "$HOME"/.local/share/plank/themes
         echo "Done"
         exit 0
         ;;
