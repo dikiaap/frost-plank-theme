8735ac83d99061b9c2e12d3661306dab78a3240f
Diki Ananta <diki1aap@gmail.com>
2018-06-14 22:38:33 +0700

    Add more description of the theme

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read -rp "Do you want to install Frost [Y/n]? " response
+read -rp "Do you want to install Frost Plank Theme? [Y/n] " response
 
 case "$response" in
     [yY][eE][sS]|[yY]|"")
